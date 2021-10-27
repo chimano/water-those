71c14b5a39570afcef5a0f1024b5ce36d988eb32
Chirac Manoukian <chiracmanoukian@hotmail.com>
2021-10-26 20:54:43 -0400

    Set up db connection

--- service/api/resources/api.go
+++ service/api/resources/api.go
@@ -1,17 +1,33 @@
 package resources
 
 import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/chimano/water-those-service/api/resources/plants"
 	"github.com/go-chi/chi"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // API provides application resources and handlers.
 type API struct {
-	Plant *PlantResource
+	Plant *plants.PlantResource
 }
 
 // NewAPI configures and returns application API.
 func NewAPI() *API {
-	plant := NewPlantResource()
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://service-db:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plantCollection := client.Database("water-those-db").Collection("plants")
+
+	plant := plants.NewPlantResource(ctx, plantCollection)
 	api := &API{
 		Plant: plant,
 	}
@@ -22,7 +38,7 @@ func NewAPI() *API {
 func (a *API) Router() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Mount("/plant", a.Plant.router())
+	r.Mount("/plant", a.Plant.Router())
 
 	return r
 }
